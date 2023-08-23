6b27eb38d620c23215a21b37a2e2cece848bce6f
NXVZBGBFBEN <NXVZBGBFBEN@gmail.com>
2023-08-23 22:50:01 +0900

    フッタに表示される内容をページごとに切り替えできるよう変更

--- src/components/footer.rs
+++ src/components/footer.rs
@@ -1,10 +1,19 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
+use crate::pages;
 
 #[function_component(Footer)]
 pub fn footer() -> Html {
+    let footnote = match use_location().unwrap().path() {
+        "/" => pages::home::footnote(),
+        _ => None
+    };
     html! {
         <>
             <footer class="mt-5 text-center">
+                if let Some(element) = footnote {
+                    { element }
+                }
                 <p>{"© 2023 NXVZBGBFBEN"}</p>
             </footer>
         </>
