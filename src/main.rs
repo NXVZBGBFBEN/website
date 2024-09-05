9042211cddfb12a58d262d77e8686b3ab71fc2f3
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-09-05 23:54:14 +0900

    scroll to top

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,23 @@ fn app() -> Html {
     html! {
         <>
             <BrowserRouter>
+                <ScrollToTop />
                 <App />
             </BrowserRouter>
         </>
     }
+}
+
+#[function_component(ScrollToTop)]
+fn scroll_to_top() -> Html {
+    let location = use_location();
+    use_effect_with(location, |_| {
+        if let Some(window) = web_sys::window() {
+            let scroll_to_options = web_sys::ScrollToOptions::new();
+            scroll_to_options.set_behavior(web_sys::ScrollBehavior::Instant);
+            scroll_to_options.set_top(0.0);
+            window.scroll_to_with_scroll_to_options(&scroll_to_options);
+        };
+    });
+    html! {}
 }
